6d7e2fc4859e3e029dbbd30423a2ae267b5cd366
RutujaJivrakh <jivrakhrb@gmail.com>
2022-11-15 17:43:16 +0530

    Addition operation

--- src/main/java/com/example/BranchConflictApplication.java
+++ src/main/java/com/example/BranchConflictApplication.java
@@ -8,6 +8,11 @@ public class BranchConflictApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BranchConflictApplication.class, args);
+	
+	
+		int a=10,b=20,c;
+		c=a+b;
+		System.out.println(c);
 	}
 
 }
